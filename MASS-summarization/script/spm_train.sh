95a4a6ab81c2d28c751dce46cdc15d2ad9196dd4
pipaek <uhmppi1@gmail.com>
2019-10-15 16:26:37 +0900

    spm train max_sentence

--- MASS-summarization/script/spm_train.sh
+++ MASS-summarization/script/spm_train.sh
@@ -3,6 +3,7 @@
 RAW_CORPUS_FILE_PATH=data/raw_korean_corpus.txt
 MODEL_PREFIX=mass_kor_32k
 VOCAB_SIZE=32000
+MAX_SENTENCE=100000000
 
 cd ..
 BASEDIR=$(pwd)
@@ -12,6 +13,7 @@ echo '>>>> Start Building SPM Model...'
 python build_tokenizer.py \
   --input=$RAW_CORPUS_FILE_PATH \
   --model_prefix=$MODEL_PREFIX \
-  --vocab_size=$VOCAB_SIZE
+  --vocab_size=$VOCAB_SIZE \
+  --mining_sentence_size=$MAX_SENTENCE
 
 echo '>>>> Building SPM Model Finished..'
